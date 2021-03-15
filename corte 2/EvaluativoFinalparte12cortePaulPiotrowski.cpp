3e8151574cc7056e9abaf21efbab25b897e43cf1
PaulPio <paulrpiotrowski@gmail.com>
2021-03-15 19:41:12 -0400

    Inicio de proyecto final

---
+++ corte 2/EvaluativoFinalparte12cortePaulPiotrowski.cpp
@@ -0,0 +1,9 @@
+//Evaluativo final del 2 corte. Primera Parte
+//Realizar un programa de bingo que tenga la siguiente estructura (5x5) (matriz)
+
+/*•	Los números deben ser aleatorios y deben ir entre (1 a 99) y 
+•	Cuando se ingrese el número y está en la tabla debe colocar un 0 o X para ir completado la tabla.
+•	Cuando se agoten todos los números debe indicar que termino y luego si quieren volver a jugar
+*/
+
+
