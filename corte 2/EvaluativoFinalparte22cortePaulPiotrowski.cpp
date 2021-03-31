74d06d1c55aa51de06966c364f00dcf72c85622b
PaulPio <paulrpiotrowski@gmail.com>
2021-03-31 17:06:05 -0400

    2 parte del Evaluativo final del 2 corte listo

--- corte 2/EvaluativoFinalparte22cortePaulPiotrowski.cpp
+++ corte 2/EvaluativoFinalparte22cortePaulPiotrowski.cpp
@@ -12,11 +12,13 @@ b.	Derrotas
 c.	Palabras usadas 
 Fecha Tope: 5 de abril
 */
+#include <sstream>
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
 #include <windows.h>
-#include <conio.h>
+#include <string.h>
+
 using namespace std;
 void ahorcado();
 
@@ -28,64 +30,99 @@ main(){
 void ahorcado(){
     //Declarar variables
     srand(time(0));
-    //string palabras[10] = {"pana", "octogenario", "estudio", "dinosaurio", "libro", "curso", "libertad", "mercado", "sentido", "comercio"};
-    int count = 10, num_palabras, opcion, j = 0; 
-    string palabras_usadas[100];
-    bool proceso = true, programa = true;
-    char carga;
+    int count = 10, num_palabras, opcion, j = 0, k= 0, victorias = 0, derrotas = 0; 
+    string palabras_usadas[100], respuesta;
+    bool proceso = true, programa = true, hangmen = true;
+    char carga;  
     string elegido; //Se asigna la palabra 
     //Mensaje de bienvenida
-    cout << "Bienvenido al juego del ahorcado user\nElija una opcion\n1.Jugar.\n2.Estadistica.\n3.Salir\n";
-    cin >> opcion;
-    system("cls");
-    while(programa){
+    do{
+        cout << "Bienvenido al juego del ahorcado user\nElija una opcion\n1.Jugar.\n2.Estadistica.\n3.Salir\n";
+        cin >> opcion;
+        system("cls");
 
     switch (opcion)
     {
         //Jugar
         case 1:
             {   //Con un bucle do-while vamos realizando el juego
-                do{
-                   
+                carga = 'Y';
+                while(carga != 'N'){
                     cout << "Elige la palabra con la que se jugara: \n";
                     cin >> elegido;
-                    int size = elegido.length();//Para contar los caracteres de cada palabra en el ahorcado
-                   
-                    for (int i = 0; i < size; i++)//bucle para la palabra del ahorcado
-                    {
-                        //int k = 1 * size;
-                        cout << "_";
-                    }
 
+                    int sizel = elegido.length();
+                    bool game = true;
+                    string pal(sizel, '_'); //Para concantenar el numero de letras de elegido
+                    
+                    int m = 0;//intentos fallidos
+                   
+                    //Bucle para medir la cantidad de intentos
+                    while(game){
+                        
+                        cout << pal << endl;    
+                        cout << "Introduzca una letra: \n";
+                        cin >> respuesta;
+                        for (int j=0; j <= sizel; j++){
+                        if(respuesta[0] == elegido[j])//para comparar la resuesta y obtener el resultado
+                            {
+                            pal[j] = elegido[j];  
+                            
+                            } 
+                        
+                        }
+                        //Para saber si la letra esta en elegido y sumar el contador de los intentos en caso de que no esté
+                        if(elegido.find(respuesta) == -1){
+                            m++;
+                        }
+                        //Si hay victoria
+                        if(pal == elegido){
+                        cout << "Has ganado esta partida";
+                        game = false;
+                        victorias++;
+                    }
+                    //Se consume todos los intentos
+                    if(m == 3){
+                        cout << "Has perdido esta partida";
+                        game = false;
+                        derrotas++;
+                    }
+                    }
+                    //Se agregan las palabras a la lista de palabras usadas con el contador j
                     palabras_usadas[j] = elegido;//Para asignar palabras usadas
                     j++;
                     
                     cout << "\nQuiere seguir jugando? (Y|N)\n";//Para saber si el user quiere seguir jugando
                     cin >> carga;
-
+                    system("cls");
                     //Condicional comparativo para saber si continuar con el programa
-                    if (j == 10){
-                        cout << "Has terminado el juego.\n";
-                        carga = 'N';
+                    if (carga == 'N'){
+                        cout << "Has terminado el juego.\n";  
                     }
                     
-                }while(carga != 'N');                
-               continue;
+                }          
+                system("pause");
+                system("cls");
+                continue;
             }
         //Estadisticas
         case 2:{
-            char parcial;
+            char parcial;//opciones del case 2 de estadisticas
             cout << "Bienvenido a la opcion de estadisticas.\nElija una opcion: \na.Victorias.\nb.Derrotas\nc.Palabras usadas.\nd.Salir\n";
             cin >> parcial;
             switch (parcial)
             {
                 //Victorias
                 case 'a':
-                    
+                    cout << "El numero de victorias en esta sesion es de " << victorias << endl;
+                    system("pause");
+                    system("cls");
                     continue;
                 //Derrotas
                 case 'b':
-
+                 cout << "El numero de victorias en esta sesion es de " << derrotas << endl;
+                    system("pause");
+                    system("cls");
                     continue;
                 //Palabras usadas
                 case 'c':
@@ -93,17 +130,19 @@ void ahorcado(){
                     {
                         cout << "Palabra [" << i << "] " << palabras_usadas[i] << endl;
                     }
-                    
+                    system("pause");
+                    system("cls");
                     continue;
                     //Salir del menu
                 case 'd':
                     cout << "Ha elegido salir del menu.\n";
+                    system("cls");
                     continue;
                 default:
                     cout << "Ha escrito mal un dato\n";
                     system("pause");
                     system("cls");
-                    break;
+                    continue;
             }
             continue;
         }
@@ -111,6 +150,8 @@ void ahorcado(){
         case 3:
             cout << "Has elegido salir del programa\n";
             programa = false;
+            system("pause");
+            system("cls");
             break;
         default:
             cout << "Has introducido mal un dato\n";
@@ -119,6 +160,6 @@ void ahorcado(){
             continue;
         }
     
-    }
+    }while(programa);
    
 }
